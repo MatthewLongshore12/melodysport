0d13790bb47147365cdde45c23a254743bcc9214
Matthew Longshore <matthewlongshore12@gmail.com>
2023-06-15 20:02:54 -0400

    basic page for services

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './userviews/Navbar';
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './userviews/LandingPage'
 import AboutPage from './userviews/AboutPage'
+import ServicesPage from './userviews/ServicesPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/services' element={<ServicesPage/>}/>
       </Routes>
     </div>
   )
