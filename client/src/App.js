0aaab0345f5d87392b3a793af3589b53e0137fbe
Matthew Longshore <matthewlongshore12@gmail.com>
2023-06-27 17:04:04 -0400

    navbar/header tried sizing with different screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import Navbar from './userviews/Navbar';
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './userviews/LandingPage'
-import AboutPage from './userviews/AboutPage'
-import ServicesPage from './userviews/ServicesPage';
-import ContactPage from './userviews/ContactPage';
+// import { Routes, Route } from 'react-router-dom'
+// import LandingPage from './userviews/LandingPage'
+// import AboutPage from './userviews/AboutPage'
+// import ServicesPage from './userviews/ServicesPage';
+// import ContactPage from './userviews/ContactPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/about' element={<AboutPage/>}/>
         <Route path='/services' element={<ServicesPage/>}/>
         <Route path='/contact' element={<ContactPage/>}/>
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
--- client/src/userviews/Navbar.js
+++ client/src/userviews/Navbar.js
@@ -1,27 +1,66 @@
-import React from 'react';
-// import '../stylesheets/navbar.css';
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import '../stylesheets/navbar.css';
 
 function Navbar() {
+  // TOGGLE MENU BUTTON
+  const[Toggle, showMenu] = useState(false)
 
   return (
-    <nav className="navbar">
-      <div className="logo">MelodysPort</div>
-      <ul className="nav-links">
-        <li>
-            <NavLink to='/'>Home</NavLink>
-        </li>
-        <li>
-            <NavLink to='/about'>About</NavLink>
-        </li>
-        <li>
-            <NavLink to='/services'>Services</NavLink>
-        </li>
-        <li>
-            <NavLink to='/contact'>Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="=nav__logo">Poo Poo</a>
+
+        <div className={Toggle ? "nav-menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i 
+          className="uil uil-times nav__close" 
+          onClick={() => showMenu(!Toggle)}></i>
+        </div>
+
+        <div 
+        className="nav_toggle" 
+        onClick={() => showMenu(!Toggle)}>
+          <i className='uil uil-apps'></i>
+        </div>
+      </nav>
+    </header>
   )
 }
 
