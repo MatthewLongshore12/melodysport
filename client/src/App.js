4096f90c525a483825031b6c8a2d555e88cafd62
Matthew Longshore <matthewlongshore12@gmail.com>
2023-08-15 12:32:16 -0400

    overhaul

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
-import './App.css';
-import Navbar from './userviews/Navbar';
-import LandingPage from './userviews/LandingPage';
+import React from 'react';
+import "./App.css";
+import Header from './userviews/header/Header';
+import Home from './userviews/home/Home';
+import About from './userviews/about/About';
+import Skills from './userviews/skills/Skills';
+import Services from './userviews/services/Services';
+import Qualification from './userviews/qualification/Qualification';
+import Work from './userviews/Portfolio/Work';
+import Testimonials from './userviews/testimonials/Testimonials';
+import Contact from './userviews/contact/Contact';
+import Footer from './userviews/footer/Footer';
+import ScrollUp from './userviews/scrollup/ScrollUp';
 
-function App() {
+const App = () => {
   return (
     <>
-    <Navbar />
+    <Header />
 
-    <main className="main">
-      <LandingPage />
+    <main className='main'>
+      <Home />
+      <About />
+      <Skills />
+      <Services />
+      <Qualification />
+      <Work />
+      <Testimonials />
+      <Contact />
     </main>
+
+    <Footer />
+    <ScrollUp />
     </>
   )
 }
 
-export default App;
+export default App
